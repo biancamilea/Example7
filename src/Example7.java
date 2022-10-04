a9b99d75864c750ef1dd43c2556faecc31985ed9
Bianca Milea <alumil2013@ieselcaminas.org>
2022-10-04 22:08:48 +0200

    Example 7

--- src/Example7.java
+++ src/Example7.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Example7 {
+import java.util.Scanner;
+class Example7 {
+    public static void main(String[] argv) {
+
+        double metros;
+        double millas;
+        Scanner scanner = new Scanner(System.in);
+        millas = scanner.nextDouble();
+        metros = millas * 1852;
+
+        System.out.println(metros);
+    }
 }
